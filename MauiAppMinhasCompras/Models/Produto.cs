24751e1e9e604ccc70cedc6f472fc22059c6b79d
Kelly Rodrigues <kellycristina80@yahoo.com.br>
2026-03-01 14:21:39 -0300

    iniciando agenda1.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -9,14 +9,27 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
-        [PrimayKey, AutoIncrement]
-        public int ID { get; set; }
-        public string Descricao { get; set; }
+        string _descricao;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
-    }
-}
-    internal class Produto
-    {
-    }
-}
+        public double Total { get => Quantidade * Preco; }
+    } // Fecha classe
+} // Fecha Namespace    }
+
+   
