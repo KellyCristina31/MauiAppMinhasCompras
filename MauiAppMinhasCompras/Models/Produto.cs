76ed4c9288ec336ad73f133789cc5a8b1b3766a8
Kelly Rodrigues <kellycristina80@yahoo.com.br>
2026-03-01 13:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimayKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+    }
+}
+    internal class Produto
+    {
+    }
+}
