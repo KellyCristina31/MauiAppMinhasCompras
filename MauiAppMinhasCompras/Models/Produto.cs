3b4993efed2b0c58bfff45af86d506f0f0066d48
Kelly Rodrigues <kellycristina80@yahoo.com.br>
2026-03-14 09:18:10 -0300

    Implementação de lista dinamica, soma e adição de produtos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,31 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
-        string _descricao;
-
+        
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao
-        {
-            get => _descricao;
-            set
-            {
-                if (value == null)
-                {
-                    throw new Exception("Por favor, preencha a descrição");
-                }
-
-                _descricao = value;
-            }
-        }
+        public string Descricao { get; set; }
+                   
         public double Quantidade { get; set; }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
